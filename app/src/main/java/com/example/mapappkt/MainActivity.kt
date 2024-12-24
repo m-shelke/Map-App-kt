9d1c68e1fc7f062c3c6e092a082cd19ecb2e2af7
AtomicFive4059 <mshelke074@gmail.com>
2024-12-24 19:09:53 +0530

    Google Map

---
+++ app/src/main/java/com/example/mapappkt/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.mapappkt
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var button : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        button = findViewById(R.id.btMapOpen)
+
+        button.setOnClickListener {
+            var intent = Intent(this,MapsActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
